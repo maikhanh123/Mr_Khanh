978ead919f4bec702fb5543047d660f0c02154a8
maikhanh123 <maikhanh123@gmail.com>
2017-11-21 13:36:04 +0700

    add comment for all products, logIn, logOut, serach for product in Store

--- public/js/app.js
+++ public/js/app.js
@@ -1,1 +1,8 @@
-angular.module('myApp', ['ngRoute', 'appRoutes', 'MainCtrl', 'NewsCtrl', 'DetailCtrl', 'StoreCtrl']);
+var app = angular.module('myApp', ['ngRoute', 'ngStorage','appRoutes', 'MainCtrl', 'NewsCtrl', 'DetailCtrl', 'StoreCtrl']);
+
+app.directive("navBar",function () {
+    return{
+        restric: "AEC",
+        templateUrl : "views/navBar.html"
+    }
+});
